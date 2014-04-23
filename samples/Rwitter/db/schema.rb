b848551416bc6fe70bfa0cbd0f5f77aaf8b638ed
memerelics <takuya21hashimoto@gmail.com>
2014-04-24 01:42:10 +0900

    install sorcery, generate default, and db:migrate

--- samples/Rwitter/db/schema.rb
+++ samples/Rwitter/db/schema.rb
@@ -11,12 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140325013007) do
+ActiveRecord::Schema.define(version: 20140423163942) do
 
   create_table "posts", force: true do |t|
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "email",            null: false
+    t.string   "crypted_password", null: false
+    t.string   "salt",             null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
